cf9fe929279deeaf1dcf2503b5733c9bca1454db
Layne Faler <laynefaler@gmail.com>
2020-02-14 13:54:54 -0800

    Updates to row

--- components/Row.jsx
+++ components/Row.jsx
@@ -15,6 +15,7 @@ export const Row = ({ children, align, justify, style = {}, ...rest }) => {
     });
     const justifyContentStyles = directionalOptions('justifyContent');
     const alignItemsStyles = directionalOptions('alignItems');
+    
     const style = Object.assign(
         { display: 'flex', flexDirection: 'row' }, 
         align && alignItemsStyles[align] ? alignItemsStyles[align] : {},
